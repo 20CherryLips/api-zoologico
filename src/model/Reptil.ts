0f8956c08d762a9ed92f35bf4d3b31e1a1a8774e
Rayssa Perdigão <rayssaperdigao20@gmail.com>
2024-01-31 16:40:06 -0300

    Iniciadas as classes Ave e Reptil

---
+++ src/model/Reptil.ts
@@ -0,0 +1,16 @@
+class Reptil extends Animal {
+    private tipo_de_escama: string;
+
+    constructor(_tipo_de_escama: string, _nome: string, _idade: number, _genero: string) {
+        super(_nome, _idade, _genero);
+        this.tipo_de_escama = _tipo_de_escama;
+    }
+
+    public setTipo_de_escama(tipo_de_escama: string): void {
+        this.tipo_de_escama = tipo_de_escama;
+    }
+
+    public getTipo_de_escama(): string {
+        return this.tipo_de_escama;
+    }
+}
